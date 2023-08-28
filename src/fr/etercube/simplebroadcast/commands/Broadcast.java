e0046407e9c13983a7def0457270e644310e20b1
trisout78 <81978700+trisout78@users.noreply.github.com>
2023-08-28 14:28:19 +0200

    Plugin fonctionnel

--- src/fr/etercube/simplebroadcast/commands/Broadcast.java
+++ src/fr/etercube/simplebroadcast/commands/Broadcast.java
@@ -1,57 +1,37 @@
 package fr.etercube.simplebroadcast.commands;
 
 import fr.etercube.simplebroadcast.utils.Strings;
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.CommandSender;
-import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.Title;
+import net.md_5.bungee.api.*;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.TabExecutor;
 
-// Importations nécessaires pour les fonctionnalités du plugin BungeeCord
+import java.util.ArrayList;
+import java.util.List;
 
 public class Broadcast extends Command {
 
-    // Crée une nouvelle commande "broadcast"
     public Broadcast() {
         super("broadcast");
     }
 
-    // Fonction exécutée lorsque la commande est utilisée
+    @Override
     public void execute(CommandSender sender, String[] args) {
-        // Vérifie si l'émetteur de la commande est un joueur
-        if ((sender instanceof ProxiedPlayer)) {
-            ProxiedPlayer p = (ProxiedPlayer) sender;
+        if (args.length < 2) {
+            sender.sendMessage(Strings.color("&cUsage: /broadcast <title|chat> <message>"));
+            return;
+        }
 
-            // Vérifie si au moins 2 arguments sont fournis
-            if (args.length >= 2) {
-                // Récupère le type de message spécifié (titre ou chat)
-                String messageType = args[1];
+        String messageType = args[0];
+        String message = String.join(" ", args).substring(messageType.length() + 1);
 
-                // Construit le message à partir des arguments restants
-                StringBuilder message = new StringBuilder();
-                for (int i = 2; i < args.length; i++) {
-                    message.append(args[i]).append(" ");
-                }
-
-                // Effectue différentes actions en fonction du type de message
-                switch (messageType) {
-                    case "title":
-                        // Diffuse un titre avec le message
-                        broadcastTitle(message.toString());
-                        break;
-                    case "chat":
-                        // Diffuse un message dans le chat
-                        broadcastChatMessage(message.toString());
-                        break;
-                    default:
-                        // Affiche un message d'erreur pour une utilisation correcte
-                        TextComponent chatMessage = new TextComponent(Strings.color("&cUsage: /broadcast <title:chat> <message>"));
-                        p.sendMessage(ChatMessageType.CHAT, chatMessage);
-                        break;
-                }
-            }
+        if (messageType.equalsIgnoreCase("title")) {
+            broadcastTitle(message);
+        } else if (messageType.equalsIgnoreCase("chat")) {
+            broadcastChatMessage(message);
+        } else {
+            sender.sendMessage(Strings.color("&cInvalid message type. Use 'title' or 'chat'."));
         }
     }
 
@@ -61,10 +41,9 @@ public class Broadcast extends Command {
                 .title(TextComponent.fromLegacyText(Strings.color("&7&l≫ &c&lANNONCE &7&l≪")))
                 .subTitle(TextComponent.fromLegacyText(message))
                 .fadeIn(10)
-                .stay(40)
+                .stay(80)
                 .fadeOut(10);
 
-        // Envoie le titre à tous les joueurs
         for (ProxiedPlayer player : ProxyServer.getInstance().getPlayers()) {
             title.send(player);
         }
@@ -74,7 +53,6 @@ public class Broadcast extends Command {
     private void broadcastChatMessage(String message) {
         TextComponent chatMessage = new TextComponent(Strings.color("&c&n&lAnnonce :&r\n" + message));
 
-        // Envoie le message dans le chat à tous les joueurs
         for (ProxiedPlayer player : ProxyServer.getInstance().getPlayers()) {
             player.sendMessage(ChatMessageType.CHAT, chatMessage);
         }
